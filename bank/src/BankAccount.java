127ff6573ffde5957b970e1f7d20f0e29cf41510
yoonsangkwak <yunsang08@naver.com>
2020-08-24 18:08:40 +0900

    java object

--- bank/src/BankAccount.java
+++ bank/src/BankAccount.java
@@ -5,17 +5,17 @@ public class BankAccount {
     // 파라미터 : 입금할 액수 (정수)
     // 리턴 : 성공여부 (불린)
     boolean deposit(int amount) {
-        if ((owner.cashAmount - amount) < 0 || amount < 0) {
+        if ((owner.getCashAmount() - amount) < 0 || amount < 0) {
             System.out.print("입금 실패입니다. ");
             System.out.print("잔고: " + balance + "원, ");
-            System.out.println("현금: " + owner.cashAmount + "원");
+            System.out.println("현금: " + owner.getCashAmount() + "원");
             return false;
         } else {
-            owner.cashAmount -= amount;
+            owner.setCashAmount(owner.getCashAmount() - amount);
             balance += amount;
             System.out.print(amount + "원 입금하였습니다.");
             System.out.print("잔고: " + balance + "원, ");
-            System.out.println("현금: " + owner.cashAmount + "원");
+            System.out.println("현금: " + owner.getCashAmount() + "원");
             return true;
         }
     }
@@ -26,14 +26,14 @@ public class BankAccount {
         if ((balance - amount) < 0 || amount < 0) {
             System.out.print("출금 실패입니다. ");
             System.out.print("잔고: " + balance + "원, ");
-            System.out.println("현금: " + owner.cashAmount + "원");
+            System.out.println("현금: " + owner.getCashAmount() + "원");
             return false;
         } else {
             balance -= amount;
-            owner.cashAmount += amount;
+            owner.setCashAmount(owner.getCashAmount() + amount);
             System.out.print(amount + "원 출금하였습니다. ");
             System.out.print("잔고: " + balance + "원, ");
-            System.out.println("현금: " + owner.cashAmount + "원");
+            System.out.println("현금: " + owner.getCashAmount() + "원");
             return true;
         }
     }
--- bank/src/BankDriver.java
+++ bank/src/BankDriver.java
@@ -2,26 +2,26 @@ public class BankDriver {
     public static void main(String[] args) {
         // 사람 선언
         Person p1 = new Person();
-        p1.name = "김신의";
-        p1.age = 28;
-        p1.cashAmount = 30000;
+        p1.setName("김신의");
+        p1.setAge(28);
+        p1.setCashAmount(30000);
 
         // 은행 계좌 생성
         BankAccount a1 = new BankAccount();
         a1.balance = 100000;
 
-        p1.account = a1;
+        p1.setBankAccount(a1);
         a1.owner = p1;
 
         Person p2 = new Person();
-        p2.name = "곽윤상";
-        p2.age = 25;
-        p2.cashAmount = 100000;
+        p2.setName("곽윤상");
+        p2.setAge(25);
+        p2.setCashAmount(100000);
 
         BankAccount a2 = new BankAccount();
         a2.balance = 500000;
 
-        p2.account = a2;
+        p2.setBankAccount(a2);
         a2.owner = p2;
 
         System.out.println(a2.deposit(30000));
