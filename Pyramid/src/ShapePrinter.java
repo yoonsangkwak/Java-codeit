f5451039c2cf4ffecde92af73c1389638bb3fe9c
yoonsangkwak <yunsang08@naver.com>
2020-08-31 17:14:05 +0900

    Pyramid

---
+++ Pyramid/src/ShapePrinter.java
@@ -0,0 +1,13 @@
+public class ShapePrinter {
+    public void printPyramid(int height) {
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= height - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
