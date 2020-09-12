056b4dbd45e0e00a30966899bb77934cef0bd8a6
yoonsangkwak <yunsang08@naver.com>
2020-09-12 15:09:45 +0900

    codebox theater

---
+++ codebox/src/Theater.java
@@ -0,0 +1,45 @@
+public class Theater {
+
+    private Seat[][] seats;
+
+    private int rowCount, colCount;
+
+    public Theater(int rowCount, int colCount) {
+        if (rowCount > 26) {
+            rowCount = 26;
+        }
+
+        seats = new Seat[rowCount][colCount];
+
+        for (int i = 0; i < rowCount; i++) {
+            for (int j = 0; j < colCount; j++) {
+                seats[i][j] = new Seat();
+            }
+        }
+
+        this.rowCount = rowCount;
+        this.colCount = colCount;
+    }
+
+
+    public void printSeatMatrix() {
+        System.out.print("  ");
+        for (int i = 1; i <= 9; i++) {
+            System.out.print("  " + i);
+        }
+        System.out.println();
+
+        for (int i = 0; i < rowCount; i++) {
+            System.out.print((char) ('A' + i) + ": ");
+            for (int j = 0; j < colCount; j++) {
+                Seat s = seats[i][j];
+                if (s.isOccupied()) {
+                    System.out.print("[O]");
+                } else {
+                    System.out.print("[ ]");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
