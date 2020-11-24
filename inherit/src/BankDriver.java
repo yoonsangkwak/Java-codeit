8cb12d993029eabcae1f835e877da6bb96abdfb2
yoonsangkwak <yunsang08@naver.com>
2020-11-24 15:46:46 +0900

    casting

---
+++ inherit/src/BankDriver.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class BankDriver {
+    public static void main(String[] args) {
+        BankAccount ba = new BankAccount();
+        MinimumBalanceAccount mba = new MinimumBalanceAccount();
+        SavingsAccount sa = new SavingsAccount();
+
+        ba.deposit(1000);
+        mba.deposit(1000);
+        sa.deposit(1000);
+
+        ArrayList<BankAccount> accounts = new ArrayList<>();
+
+        accounts.add(ba);
+        accounts.add(mba);
+        accounts.add(sa);
+
+        for (BankAccount account : accounts) {
+            account.deposit(1000);
+            if (account instanceof SavingsAccount) {
+                ((SavingsAccount) account).addInterest();
+            }
+        }
+    }
+}
