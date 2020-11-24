46c829ef1f1ca8854009c0e2ebd7cdf963c05c55
yoonsangkwak <yunsang08@naver.com>
2020-11-24 18:04:09 +0900

    comparable interface

---
+++ comparable/src/Person.java
@@ -0,0 +1,19 @@
+public class Person implements Comparable<Person> {
+    public final String name;
+    public final int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return name + ": " + age + "세";
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return name.compareTo(o.name);
+    }
+}
