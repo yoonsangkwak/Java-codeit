0b365fc321847fc1a4231fec5671e10870ec9690
yoonsangkwak <yunsang08@naver.com>
2020-11-24 15:07:01 +0900

    card shuffler

---
+++ cardgame/src/Deck.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class Deck {
+    private ArrayList<Card> cards = new ArrayList<>();
+
+    public ArrayList<Card> getCards() {
+        return cards;
+    }
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    public void print() {
+        for (Card card : cards) {
+            System.out.println(card.toString());
+        }
+    }
+
+    public void shuffle() {
+        // Collections.shuffle(cards);
+        Random random = new Random();
+
+        for (int i = 0; i < cards.size(); i++) {
+            int randIndex = random.nextInt(cards.size());
+            Card temp = cards.get(i);
+            cards.set(i, cards.get(randIndex));
+            cards.set(randIndex, temp);
+        }
+    }
+
+    public Deck deal(int count) {
+        Deck hand = new Deck();
+        if (count == 5) {
+            for (int i = 0; i < count; i++) {
+                hand.addCard(cards.remove(i));
+            }
+        }
+        return hand;
+    }
+}
